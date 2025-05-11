70bb37c47f9d681e47dfc3f22bcab411d612d5d7
Truc Thi-Thanh Le <143344728+funaLee@users.noreply.github.com>
2025-05-11 13:45:39 +0700

    Fix dataset card loading

--- js/dataset-render.js
+++ js/dataset-render.js
@@ -1,24 +1,35 @@
 function renderDatasetCard(containerSelector, dataset) {
-  const template = document.getElementById('dataset-card-template');
-  if (!template) return;
+  // Wait for template to be loaded
+  const waitForTemplate = () => {
+    const template = document.getElementById('dataset-card-template');
+    if (template) {
+      const clone = template.content.cloneNode(true);
 
-  const clone = template.content.cloneNode(true);
+      clone.querySelector('.dataset-image').src = dataset.image;
+      clone.querySelector('.dataset-image').alt = dataset.title;
+      clone.querySelector('.dataset-title').textContent = dataset.title;
+      clone.querySelector('.dataset-author-name').textContent = dataset.author;
+      clone.querySelector('.dataset-link').href = dataset.paperUrl;
+      clone.querySelector('.dataset-link').textContent = dataset.paperTitle;
+      clone.querySelector('.dataset-link-button').href = dataset.homepage;
 
-  clone.querySelector('.dataset-image').src = dataset.image;
-  clone.querySelector('.dataset-image').alt = dataset.title;
-  clone.querySelector('.dataset-title').textContent = dataset.title;
-  clone.querySelector('.dataset-author-name').textContent = dataset.author;
-  clone.querySelector('.dataset-link').href = dataset.paperUrl;
-  clone.querySelector('.dataset-link').textContent = dataset.paperTitle;
-  clone.querySelector('.dataset-link-button').href = dataset.homepage;
+      const tagsContainer = clone.querySelector('.dataset-tags');
+      dataset.tags.forEach(tag => {
+        const span = document.createElement('span');
+        span.className = 'tag';
+        span.textContent = tag;
+        tagsContainer.appendChild(span);
+      });
 
-  const tagsContainer = clone.querySelector('.dataset-tags');
-  dataset.tags.forEach(tag => {
-    const span = document.createElement('span');
-    span.className = 'tag';
-    span.textContent = tag;
-    tagsContainer.appendChild(span);
-  });
+      const container = document.querySelector(containerSelector);
+      if (container) {
+        container.appendChild(clone);
+      }
+    } else {
+      // If template is not loaded yet, wait and try again
+      setTimeout(waitForTemplate, 100);
+    }
+  };
 
-  document.querySelector(containerSelector).appendChild(clone);
+  waitForTemplate();
 }
